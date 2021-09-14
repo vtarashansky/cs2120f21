4ab65263b25af2e7efc408448510de8e34c7e52f
Vlad Tarashansky <qbu8hp@virginia.edu>
2021-09-14 20:09:09 +0000

    homework

--- src/mywork/practice_1.lean
+++ src/mywork/practice_1.lean
@@ -1,4 +1,12 @@
 /-
+eman
+jmb4zz https://github.com/jbrady1203/cs2120f21.git
+hae3ra https://github.com/jackdebk/cs2120f21.git
+qbu8hp https://github.com/vtarashansky/cs2120f21.git
+-/
+
+
+/-
 EQUALITY
 -/
 
@@ -13,6 +21,12 @@ this question that makes it much easier to answer than it might
 at first appear.
 -/
 
+/-
+Assume x, y, z, and w are arbitrary objects of a type T. Assume x=y
+y=z and, w=z. z=w through the theorem of symmetry in equality applied to w=z.
+-/
+
+
 /- #2
 Give a formal statement of the conjecture (proposition) from
 #1 by filling in the "hole" in the following definition. The
@@ -22,12 +36,7 @@ all propositions in Lean).
 -/
 
 def prop_1 : Prop := 
-  ∀ (T:Type)
-    (x y z w:T),
-    x = y → 
-    y = z → 
-    w = z →
-    z = w 
+  ∀ (T:Type) (x y z w : T), x=y → y=z → w=z → z=w
 
 
 /- #3 (extra credit)
@@ -40,10 +49,11 @@ again, called eq.refl, eq.subst, eq.symm, eq.trans.
 theorem prop_1_proof : prop_1 := 
 begin
   unfold prop_1,
-  assume T x y z w e1 e2 e3,
-  rw e3
-  /-apply eq.subst e3,
-  exact eq.refl w-/
+  assume T x y z w,
+  assume e1,
+  assume e2,
+  assume e3,
+  apply eq.symm e3,
 end
 
 /-
@@ -57,12 +67,17 @@ what do you do? (I'm being a little informal in leaving out the
 type of X.) 
 -/
 
+/- 
+Assume a arbitrary object of an arbititrary type. If you prove the rule 
+for that object of that type, it applies to all types and objects
+-/
+
 /- #5
 Suppose you have a proof, let's call it pf, of the proposition,
 (∀ x, P x), and you need a proof of P t, for some particular t.
 Write an expression then uses the elimination rule for ∀ to get
 such a proof. Complete the answer by replacing the underscores
-in the following expression: ( _ _ ). 
+in the following expression: (P t := pf t).
 -/
 
 /-
@@ -86,7 +101,7 @@ Hint: put parenthesis around "n + 1" in your answer.
 -/
 
 def successor_of_even_is_odd : Prop := 
-  _
+  ∀ (n : ℕ), ev n → odd (n+1)
 
 /- #7
 Suppose that "its_raining" and "the_streets_are_wet" are
@@ -98,7 +113,7 @@ by filling in the hole
 
 axioms (raining streets_wet : Prop)
 
-axiom if_raining_then_streets_wet : _
+axiom if_raining_then_streets_wet : raining → streets_wet
   
 
 /- #9
@@ -112,7 +127,10 @@ you are asked to use the elimination rule for →.
 axiom pf_raining : raining
 
 example : streets_wet :=
- _
+  begin
+    apply if_raining_then_streets_wet,
+    exact pf_raining,
+  end
 
 /- 
 AND: ∧
@@ -159,7 +177,11 @@ theorem and_associative :
 begin
   intros P Q R h,
   have p : P := and.elim_left h,
-  
+  have qr : Q ∧ R := and.elim_right h,
+  have q : Q := and.elim_left qr,
+  have r : R := and.elim_right qr,
+  have pq: P∧Q := and.intro p q,
+  apply and.intro pq r,
 end
 
 /- #11
@@ -177,6 +199,18 @@ _____ to a proof of (P ∧ Q) and a proof of R.
 What remains, then, is to obtain these proofs.
 But this is easily done by the application of
 ____ to ____. QED. 
+
+
+Proof. We assume that P, Q, and R are arbitrary but
+specific propositions, and that we have a proof called 
+p_qr of (P ∧ (Q ∧ R)) by application of ∧ and →
+introduction. What reamins to be proved is ((P ∧ Q) ∧ R).
+We can first use the elimination rule of and to seperate 
+to a proof of P and a proof of Q ∧ R. From there, the 
+elimination rule can be applied again to obtain a proof of Q 
+and a proof of R. Through the introduction rule of and we 
+obtain a proof of P ∧ Q. Finally through the introduction 
+rule of and we can obtain ((P ∧ Q) ∧ R). QED.
 -/
 
 
@@ -189,4 +223,4 @@ Note the difference in naming relative to
 the definitions we give in this file.
 -/
 #check @and.comm
-#check @and.assoc
+#check @and
---
+++ src/mywork/review9-13.lean
@@ -0,0 +1,55 @@
+
+
+
+
+namespace implies
+
+axioms ( P Q : Prop)
+
+def if_P_is_true_then_so_is_Q: Prop:= P → Q
+
+--assume P is true
+--assume we have a proof of P(p)
+axiom p:P
+
+axiom pq: P → Q
+-- assume that we have a proof, pq, of P → Q
+
+--intro for implies: assume premise, show conclusion
+--elimination rule for implies: 
+
+#check pq
+
+/-
+Suppose P and Q are propositionns and you 
+know that P → Q and that P are both True
+Show that Q is true
+
+Proof: apply the truth of P → Q to the 
+truth of P to derive the truth of Q
+
+Proof: by the  elimination rule for → with
+pq applied to p
+
+proof: by "modus ponnens". QED
+-/
+end implies
+namespace all
+/-
+FORALL
+-/
+
+axioms
+  (T:Type)
+  (P:T→ Prop)
+  (t:T)
+  (pf:∀ (x:T), P x)
+  --Does t have property P?    
+   --Yes bc all x of type T have P x so t must have P x
+  --
+
+  example: P t:= pf t
+
+  #check pf t
+  
+end all
